138b8cb270d3762d078444101b464e47afa03150
David Jensenius <djensenius@gmail.com>
2018-11-12 22:40:43 -0500

    Add linting

--- src/js/components/presentational/Input.js
+++ src/js/components/presentational/Input.js
@@ -1,6 +1,10 @@
+// @flow
 import React from "react";
 import PropTypes from "prop-types";
-const Input = ({ label, text, type, id, value, handleChange }) => (
+
+const Input = ({
+  label, text, type, id, value, handleChange,
+}) => (
   <div className="form-group">
     <label htmlFor={label}>{text}</label>
     <input
@@ -19,6 +23,6 @@ Input.propTypes = {
   type: PropTypes.string.isRequired,
   id: PropTypes.string.isRequired,
   value: PropTypes.string.isRequired,
-  handleChange: PropTypes.func.isRequired
+  handleChange: PropTypes.func.isRequired,
 };
 export default Input;

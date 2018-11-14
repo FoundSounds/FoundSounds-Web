4a4dfde4ba3297480b09b0f65e89792bdfd43a0f
David Jensenius <djensenius@gmail.com>
2018-11-13 20:25:29 -0500

    Add relay

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
-import FormContainer from "./js/components/container/FormContainer";
+// @flow
+import React from "react";
+import { graphql, QueryRenderer } from "react-relay";
+import environment from "./api";
+
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <QueryRenderer
+        environment={environment}
+        query={graphql`
+          query userQuery($id: ID!) {
+            id
+          }
+        `}
+        variables={{}}
+        render={({ error, props }) => {
+          if (error) {
+            return <div>Error!</div>;
+          }
+          if (!props) {
+            return <div>Loading...</div>;
+          }
+          return (
+            <div>
+              User ID: { props.viewer.id }
+            </div>
+          );
+        }}
+      />
+    );
+  }
+}

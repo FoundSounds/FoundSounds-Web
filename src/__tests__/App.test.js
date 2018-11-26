c9cf40ef2ef4d3c9d12c36ea7fe7c1198a8caf3e
David Jensenius <djensenius@gmail.com>
2018-11-25 20:51:26 -0500

    Refactor tests to be cleaner

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,24 +1,17 @@
 // @flow
 import React from "react";
 import ReactDOM from "react-dom";
-import { render, wait } from "react-testing-library";
-import fetch from "node-fetch";
+import { render, waitForElement } from "react-testing-library";
 import App from "../App";
+import SoundContainer from "../components/container/SoundContainer";
 import queryMock from "../__testUtils__/queryMock";
-
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
 
 describe("App", () => {
   let mockAppQueryData;
 
-  beforeEach(() => {
+  beforeEach(async () => {
     // Make sure mock data is always fresh for each test run
-    global.fetch = fetch;
-    queryMock.setup("http://localhost:3000/graphql");
+    queryMock.setup("https://dfj.ngrok.io/graphql");
     mockAppQueryData = {
       sound: {
         id: "1",
@@ -30,45 +23,40 @@ describe("App", () => {
         },
       },
     };
-  });
-
+  }, 500);
   describe("First screen", () => {
-    it("should render content when given a successful response", async () => {
-    /**
-     * App fetches the query AppQuery. Here we mock it, telling it to return our mock
-     * data declared above.
-     */
+    it("renders without crashing", () => {
       queryMock.mockQuery({
         name: "SoundContainerQuery",
         data: mockAppQueryData,
       });
-
-      /**
-       * We mount the app and wait for our element that displays the app's content
-       * to be visible.
-       */
-
-      const r = render(<App />);
-
-      // The ProfileDisplayer displays the name, so we wait for that to appear
-      await wait(() => r.getByText("Really great sound!"));
+      const div = document.createElement("div");
+      ReactDOM.render(<App />, div);
+      ReactDOM.unmountComponentAtNode(div);
     });
 
-    it("should render an error screen when request fails", async () => {
-      /**
-        * Here we instead want to make sure that we show an error screen when requests fail,
-      * so we tell graphql-query-test-mock to fail the request and return a status 500,
-      * and then wait for our error screen to appear.
-      */
-
+    it("should render content when given a successful response", async () => {
       queryMock.mockQuery({
-        name: "AppQuery",
+        name: "SoundContainerQuery",
+        data: mockAppQueryData,
+      });
+      const { getByText } = render(<SoundContainer id="15" />);
+      expect(getByText("Loading...")).toBeTruthy();
+      await waitForElement(() => getByText("Really great sound!"));
+      expect(getByText("Really great sound!")).toBeTruthy();
+    }, 500);
+
+    it("should render an error screen when request fails", async () => {
+      queryMock.mockQuery({
+        name: "SoundContainerQuery",
         data: {},
         status: 500,
       });
 
-      const r = render(<App />);
-      await wait(() => r.getByText(/Error!/));
-    });
+      const { getByText } = render(<App />);
+      expect(getByText("Loading...")).toBeTruthy();
+      await waitForElement(() => getByText("Error!"));
+      expect(getByText("Error!")).toBeTruthy();
+    }, 500);
   });
 });
